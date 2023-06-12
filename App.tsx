71aa9fa1d0a8c2e3d0130b3962d14289b63aa064
Daniel Ryan <ryannobusiness@gmail.com>
2023-06-12 20:56:59 +0100

    routes

--- App.tsx
+++ App.tsx
@@ -1,17 +1,34 @@
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import PrivacyPolicy from './components/onboarding/PrivacyPolicy';
+
+// Onboarding components
 import Intro from './components/onboarding/Intro';
-import React from 'react';
+import PrivacyPolicy from './components/onboarding/PrivacyPolicy';
 import LanguageSelect from './components/onboarding/LanguageSelect';
 import ShopFront from './components/onboarding/ShopFront';
 import VerifyAge from './components/onboarding/VerifyAge';
+
+// Shop components
 import VapeScreen from './components/shop/VapeScreen';
 import JuiceScreen from './components/shop/JuiceScreen';
 import NonDisposableScreen from './components/shop/NonDisposableScreen';
-import LostConnection from './components/anomalies/LostConnection';
 import BrandVarieties from './components/shop/BrandVarieties';
+
+// Anomaly components
+import LostConnection from './components/anomalies/LostConnection';
+import NotFoundScreen from './components/anomalies/NotFoundScreen';
+
+// Sales components
+import ConfirmationPage from './components/sales/ConfirmationPage';
+import DeliveryAddress from './components/sales/DeliveryAddress';
+import FormInput from './components/sales/FormInput';
+import ProductPage from './components/shop/ProductPage';
+import JuiceProductPage from './components/shop/JuiceProductPage';
+import NonDisposableProductPage from './components/shop/NonDisposableProductPage';
+
+// Subscriptions components
 import CancelConfirm from './components/subscriptions/CancelConfirm';
 import CancelMembership from './components/subscriptions/CancelMembership';
 import ChangeAddress from './components/subscriptions/ChangeAddress';
@@ -19,53 +36,40 @@ import ChangeFlavours from './components/subscriptions/ChangeFlavours';
 import ManageSubscription from './components/subscriptions/ManageSubscription';
 import SubVapeScreen from './components/subscriptions/SubVapeScreen';
 import ChooseFlavours from './components/subscriptions/ChooseFlavours';
-import ConfirmationPage from './components/sales/ConfirmationPage';
-import DeliveryAddress from './components/sales/DeliveryAddress';
-import FormInput from './components/sales/FormInput';
 
 export default function App() {
-
-
   const Stack = createStackNavigator();
 
-
-
   return (
-       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Intro">
-            <Stack.Screen name="Intro" component={Intro} />
-            <Stack.Screen name="PrivacyPolicy" component={PrivacyPolicy} />
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Intro">
+        <Stack.Screen name="Intro" component={Intro} />
+        <Stack.Screen name="PrivacyPolicy" component={PrivacyPolicy} />
+        <Stack.Screen name="LanguageSelect" component={LanguageSelect} />
+        <Stack.Screen name="ShopFront" component={ShopFront} />
+        <Stack.Screen name="VerifyAge" component={VerifyAge} />
+        <Stack.Screen name="VapeScreen" component={VapeScreen} />
+        <Stack.Screen name="JuiceScreen" component={JuiceScreen} />
+        <Stack.Screen name="NonDisposableScreen" component={NonDisposableScreen} />
+        <Stack.Screen name="LostConnection" component={LostConnection} />
+        <Stack.Screen name="ConfirmationPage" component={ConfirmationPage} />
+        <Stack.Screen name="DeliveryAddress" component={DeliveryAddress} />
+        <Stack.Screen name="FormInput" component={FormInput} />
+        <Stack.Screen name="BrandVarieties" component={BrandVarieties} />
+        <Stack.Screen name="CancelConfirm" component={CancelConfirm} />
+        <Stack.Screen name="CancelMembership" component={CancelMembership} />
+        <Stack.Screen name="ChangeAddress" component={ChangeAddress} />
+        <Stack.Screen name="ChangeFlavours" component={ChangeFlavours} />
+        <Stack.Screen name="ChooseFlavours" component={ChooseFlavours} />
+        <Stack.Screen name="ManageSubscription" component={ManageSubscription} />
+        <Stack.Screen name="SubVapeScreen" component={SubVapeScreen} />
+        <Stack.Screen name="NotFoundScreen" component={NotFoundScreen} />
 
-            <Stack.Screen name="LanguageSelect" component={LanguageSelect} />
-            <Stack.Screen name="ShopFront" component={ShopFront} />
-            <Stack.Screen name="VerifyAge" component={VerifyAge} />
-
-            <Stack.Screen name="VapeScreen" component={VapeScreen} />
-            <Stack.Screen name="JuiceScreen" component={JuiceScreen} />
-            <Stack.Screen name="NonDisposableScreen" component={NonDisposableScreen} />
-            <Stack.Screen name="LostConnection" component={LostConnection} />
-
-            <Stack.Screen name="ConfirmationPage" component={ConfirmationPage} />
-            <Stack.Screen name="DeliveryAddress" component={DeliveryAddress} />
-            <Stack.Screen name="FormInput" component={FormInput} />
-
-
-            <Stack.Screen name="BrandVarieties" component={BrandVarieties} />
-
-            <Stack.Screen name="CancelConfirm" component={CancelConfirm} />
-            <Stack.Screen name="CancelMembership" component={CancelMembership} />
-            <Stack.Screen name="ChangeAddress" component={ChangeAddress} />
-            <Stack.Screen name="ChangeFlavours" component={ChangeFlavours} />
-            <Stack.Screen name="ChooseFlavours" component={ChooseFlavours} />
-            <Stack.Screen name="ManageSubscription" component={ManageSubscription} />
-            <Stack.Screen name="SubVapeScreen" component={SubVapeScreen} />
-
-
-
-
-
-         </Stack.Navigator>
-        </NavigationContainer>
+        <Stack.Screen name="ProductPage" component={ProductPage} />
+        <Stack.Screen name="JuiceProductPage" component={JuiceProductPage} />
+        <Stack.Screen name="NonDisposableProductPage" component={NonDisposableProductPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
